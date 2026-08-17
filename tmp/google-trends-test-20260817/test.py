2b898e9911104a15d288306dc2380e422de7d478
lunchyum <theilunch@gmail.com>
2026-08-17 18:26:52 +0900

    Add one-query Google Trends diagnostic

---
+++ tmp/google-trends-test-20260817/test.py
@@ -0,0 +1,4 @@
+from pytrends.request import TrendReq
+p=TrendReq(hl='ko',tz=540,timeout=(10,30),retries=0,backoff_factor=0)
+p.build_payload(['무야호'],cat=0,timeframe='2004-01-01 2026-08-15',geo='KR',gprop='websearch')
+print(p.interest_over_time().tail().to_string())
